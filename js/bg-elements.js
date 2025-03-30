8cf2215191c4f115f5eba141a8ed5c3b4208c069
Valeri-dragon <yakontekst01@yandex.ru>
2025-03-30 22:33:31 +0300

    modified:   js/bg-elements.js

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -2,7 +2,7 @@ const setBg=()=>{
   const elements = document.querySelectorAll(".set-bg");
 
 elements.forEach((elem) => {
-    elem.style.backgroundImage = `url(../${elem.dataset.setbg})`;
+    elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
 });
 }
 setBg()
